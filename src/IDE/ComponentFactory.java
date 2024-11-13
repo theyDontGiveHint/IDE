f1418939726a9feabb9eda4913d1621abff666b2
준서 <junseokr@icloud.com>
2024-11-13 17:46:18 +0900

    FEAT : Java Editor Class

---
+++ src/IDE/ComponentFactory.java
@@ -0,0 +1,13 @@
+package IDE;
+
+import javax.swing.*;
+
+public class ComponentFactory {
+    public static JButton createButton(String text) {
+        return new JButton(text);
+    }
+
+    public static JMenuBar createMenuBar(String [] items) {
+
+    }
+}
--- src/IDE/Interface.java
+++ src/IDE/Interface.java
@@ -1,74 +1,26 @@
 package IDE;
 
-import java.io.IOException;
+import javax.swing.*;
+import java.awt.*;
 
-public class Interface {
-    /**
-     * 메인 화면을 출력합니다.
-     */
-    public static void showMainScreen(UploadFile file) {
-        if (file == null) {
-            System.out.print(
-                    """
-                    ########################################
-                    1. Java File Upload
-                    2. Compile
-                    3. Run
-                    4. Reset
-                    5. Show Compile Error
-                    6. Exit
-                    ########################################
-                    """
-            );
-        } else {
-            System.out.printf(
-                """
-                ########################################
-                1. Java File Upload
-                2. Compile
-                3. Run
-                4. Reset
-                5. Show Compile Error
-                6. Exit
-                
-                [ Uploaded File ]
-                %s
-                ########################################
-                """,
-                file.fileName
-            );
-        }
+public class Interface extends JFrame {
+    private JButton browseButton = new JButton("Browse");
+    private JButton openButton = new JButton("Open");
+    private JButton compileButton = new JButton("Compile");
+    private JButton saveButton = new JButton("Save");
+    private JButton saveErrorsButton = new JButton("Save Errors");
+    private JButton deleteButton = new JButton("Delete");
+    private JButton clearButton = new JButton("Clear");
+
+    public Interface() {
+        setTitle("Java Editor");
+        setSize(1280, 800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
     }
 
-    /**
-     * 업로드 화면을 출력합니다.
-     */
-    public static void showUploadScreen() {
-        System.out.print(
-            """
-            ########################################
-            [ 파일 업로드 ]
-            main 함수가 존재하는 Java 파일을 업로드하세요.
-            같은 디렉토리의 패키지 단위까지 인식 가능합니다.
-            
-            - 파일 전체 경로 입력
-            - 파일을 끌어다 터미널에 놓기
-            위 두 가지 방법으로 파일을 업로드하세요.
-            ########################################
-            """
-        );
-    }
-
-    /**
-     * 터미널을 초기화합니다.
-     */
-    public static void clearScreen() {
-        try {
-            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else {
-                new ProcessBuilder("clear").inheritIO().start().waitFor();
-            }
-        } catch (IOException | InterruptedException _) {}
+    private JButton createButton(String text) {
+        JButton button = new JButton(text);
+        return button;
     }
 }
